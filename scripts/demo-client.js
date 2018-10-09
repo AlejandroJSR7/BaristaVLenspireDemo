1423c4fc5ea16b28155d075a7090f067ad04e3c3
AlejandroJSR7 <AlejandroJSR7@Gmail.com>
2018-10-09 13:01:53 -0500

    Add animation to anchors

--- scripts/demo-client.js
+++ scripts/demo-client.js
@@ -1,3 +1,23 @@
 (function() {
   console.log('_demo-client.js');
+
+  $(".list-group-item a[href^='#']").on('click', function(e) {
+
+    // prevent default anchor click behavior
+    e.preventDefault();
+ 
+    // store hash
+    var hash = this.hash;
+ 
+    // animate
+    $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 300, function(){
+ 
+        // when done, add hash to url
+        // (default click behaviour)
+        window.location.hash = hash;
+      });
+ 
+ });
 })();
